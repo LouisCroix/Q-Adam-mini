4a561949d9032f37af49ab445df2e16600aad2d5
Chaohao Yang <121090688@link.cuhk.edu.cn>
2025-05-24 22:12:00 +0800

    second

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-#SBATCH -J q-adam-mini
-#SBATCH -A L00120230003
-#SBATCH -p p-A800
-#SBATCH -N 1
-#SBATCH -c 4
-#SBATCH --gres=gpu:1
-
-
 python3 ../Fine-Tuning-LLaMA-2-on-GSM8K/generate_eval.py > gsm_adamw_lora_2e-5_128_256_eval
